87e7a30bfb14cb6bc6b893a6da597d52a765729d
Francesco Bozzotta (aka xFr4xx) <francesco.bozzotta@gmail.com>
2024-08-06 19:16:12 +0200

    add bot messages in history

--- src/main.ts
+++ src/main.ts
@@ -18,26 +18,26 @@ type Context = {
 };
 
 export default async ({ req, res, log, error }: Context) => {
+  log('connect to appwrite api');
+  const client = new Client()
+    .setEndpoint(process.env.APPWRITE_ENDPOINT!)
+    .setProject(process.env.APPWRITE_PROJECT_ID!)
+    .setKey(process.env.APPWRITE_API_KEY!);
+  let datastore = new Databases(client);
+  let chat = await datastore.listDocuments(
+    process.env.APPWRITE_DATABASE_ID!,
+    process.env.APPWRITE_TABLE_CHATS_ID!,
+    [
+      Query.equal('channel', 'telegram'),
+      Query.equal('chat_id', String(req.body.message.chat.id)),
+      Query.limit(1),
+    ]
+  );
   const telegram_token = req.headers['x-telegram-bot-api-secret-token'];
   try {
     if (telegram_token === process.env.APPWRITE_API_KEY!) {
       log('connect to Telegram Bot');
       const bot = new Telegraf(process.env.TELEGRAM_TOKEN!);
-      log('connect to appwrite api');
-      const client = new Client()
-        .setEndpoint(process.env.APPWRITE_ENDPOINT!)
-        .setProject(process.env.APPWRITE_PROJECT_ID!)
-        .setKey(process.env.APPWRITE_API_KEY!);
-      let datastore = new Databases(client);
-      let chat = await datastore.listDocuments(
-        process.env.APPWRITE_DATABASE_ID!,
-        process.env.APPWRITE_TABLE_CHATS_ID!,
-        [
-          Query.equal('channel', 'telegram'),
-          Query.equal('chat_id', String(req.body.message.chat.id)),
-          Query.limit(1),
-        ]
-      );
 
       switch (req.body.message.text) {
         case '/start':
@@ -129,6 +129,18 @@ export default async ({ req, res, log, error }: Context) => {
           action.action === 'talk' &&
           action.channel === 'telegram'
         ) {
+          log('add message in conversation');
+
+          datastore.createDocument(
+            process.env.APPWRITE_DATABASE_ID!,
+            process.env.APPWRITE_TABLE_MESSAGES_ID!,
+            ID.unique(),
+            {
+              message: action.payload.value,
+              bot: true,
+              chat: chat.documents[0].$id,
+            }
+          );
           log('connect to Telegram Bot');
           const bot = new Telegraf(process.env.TELEGRAM_TOKEN!);
           log(`sent message to telegram channel`);
@@ -151,7 +163,7 @@ export default async ({ req, res, log, error }: Context) => {
     if (req.method === 'GET') {
       return res.send('Silicia - Giul-IA BOT - telegram gateway');
     }
- } catch (e: any) {
+  } catch (e: any) {
     error(JSON.stringify(e));
   }
   return res.empty();
