4fe1dbdf51ba213a3470dec558251104827c9aee
Francesco Bozzotta (aka xFr4xx) <francesco.bozzotta@gmail.com>
2024-08-06 23:37:13 +0200

    xxx

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Client, Databases, Query, ID } from 'node-appwrite';
+import { Client, Databases, Query, ID, Models } from 'node-appwrite';
 import { Telegraf } from 'telegraf';
 
 //import * as process from './env.js';
@@ -20,160 +20,166 @@ type Context = {
 export default async ({ req, res, log, error }: Context) => {
   const telegram_token = req.headers['x-telegram-bot-api-secret-token'];
   //try {
-    if (telegram_token === process.env.APPWRITE_API_KEY!) {
-      log('connect to Telegram Bot');
-      const bot = new Telegraf(process.env.TELEGRAM_TOKEN!);
-      log('connect to appwrite api');
-      const client = new Client()
-        .setEndpoint(process.env.APPWRITE_ENDPOINT!)
-        .setProject(process.env.APPWRITE_PROJECT_ID!)
-        .setKey(process.env.APPWRITE_API_KEY!);
-      let datastore = new Databases(client);
-      let chat = await datastore.listDocuments(
-        process.env.APPWRITE_DATABASE_ID!,
-        process.env.APPWRITE_TABLE_CHATS_ID!,
-        [
-          Query.equal('channel', 'telegram'),
-          Query.equal('chat_id', String(req.body.message.chat.id)),
-          Query.limit(1),
-        ]
-      );
-      switch (req.body.message.text) {
-        case '/start':
-          log('present the bot');
+  if (telegram_token === process.env.APPWRITE_API_KEY!) {
+    log('connect to Telegram Bot');
+    const bot = new Telegraf(process.env.TELEGRAM_TOKEN!);
+    log('connect to appwrite api');
+    const client = new Client()
+      .setEndpoint(process.env.APPWRITE_ENDPOINT!)
+      .setProject(process.env.APPWRITE_PROJECT_ID!)
+      .setKey(process.env.APPWRITE_API_KEY!);
+    let datastore = new Databases(client);
+    let chat = await datastore.listDocuments(
+      process.env.APPWRITE_DATABASE_ID!,
+      process.env.APPWRITE_TABLE_CHATS_ID!,
+      [
+        Query.equal('channel', 'telegram'),
+        Query.equal('chat_id', String(req.body.message.chat.id)),
+        Query.limit(1),
+      ]
+    );
+    switch (req.body.message.text) {
+      case '/start':
+        log('present the bot');
+        bot.telegram.sendMessage(
+          String(req.body.message.chat.id),
+          'Hello everyone! I am an AI under development, with learning and conversational abilities. To start interacting with me, type the magic word. 😉 What is the magic word?'
+        );
+        break;
+      case 'start@imitation@game':
+        log('Registrazione Bot');
+        if (chat.total === 0) {
+          log('User not present');
+          const new_user = {
+            es: { fear: 0 },
+            ltm: [
+              {
+                key: 'first_name_user',
+                value: [req.body.message.from.first_name],
+              },
+              {
+                key: 'last_name_user',
+                value: [req.body.message.from.last_name],
+              },
+              {
+                key: 'prefered_language_user',
+                value: [req.body.message.from.language_code],
+              },
+              {
+                key: 'username_user',
+                value: [req.body.message.from.username],
+              },
+            ],
+            name: req.body.message.from.username,
+            chats: [
+              {
+                channel: 'telegram',
+                chat_id: String(req.body.message.chat.id),
+              },
+            ],
+          };
+          log(`write new user`);
+          log(JSON.stringify(new_user));
+          await datastore.createDocument(
+            process.env.APPWRITE_DATABASE_ID!,
+            process.env.APPWRITE_TABLE_PROFILES_ID!,
+            ID.unique(),
+            new_user
+          );
+          log(`user created`);
           bot.telegram.sendMessage(
             String(req.body.message.chat.id),
-            'Hello everyone! I am an AI under development, with learning and conversational abilities. To start interacting with me, type the magic word. 😉 What is the magic word?'
+            "You managed to say the magic word and now we can finally start interacting. 🤖 If you're curious to see what commands I can execute, visit t.me/giul_ia_actions_bot, while if you want to take a look at my thought process, I'm waiting for you at t.me/giul_ia_think_bot. To interact with me, just write in this chat! 😉 Up until now, you've been shown prerendered text, now the magic happens."
           );
-          break;
-        case 'start@imitation@game':
-          log('Registrazione Bot');
-          if (chat.total === 0) {
-            log('User not present');
-            const new_user = {
-              es: { fear: 0 },
-              ltm: [
-                {
-                  key: 'first_name_user',
-                  value: [req.body.message.from.first_name],
-                },
-                {
-                  key: 'last_name_user',
-                  value: [req.body.message.from.last_name],
-                },
-                {
-                  key: 'prefered_language_user',
-                  value: [req.body.message.from.language_code],
-                },
-                {
-                  key: 'username_user',
-                  value: [req.body.message.from.username],
-                },
-              ],
-              name: req.body.message.from.username,
-              chats: [
-                {
-                  channel: 'telegram',
-                  chat_id: String(req.body.message.chat.id),
-                },
-              ],
-            };
-            log(`write new user`);
-            log(JSON.stringify(new_user));
-            await datastore.createDocument(
-              process.env.APPWRITE_DATABASE_ID!,
-              process.env.APPWRITE_TABLE_PROFILES_ID!,
-              ID.unique(),
-              new_user
-            );
-            log(`user created`);
-            bot.telegram.sendMessage(
-              String(req.body.message.chat.id),
-              "You managed to say the magic word and now we can finally start interacting. 🤖 If you're curious to see what commands I can execute, visit t.me/giul_ia_actions_bot, while if you want to take a look at my thought process, I'm waiting for you at t.me/giul_ia_think_bot. To interact with me, just write in this chat! 😉 Up until now, you've been shown prerendered text, now the magic happens."
-            );
-          } else {
-            bot.telegram.sendMessage(
-              String(req.body.message.chat.id),
-              'Welcome Back to Giulia BOT'
-            );
-            log(`user already in database`);
-          }
-          break;
-        default:
-          if (chat.total > 0) {
-            datastore.createDocument(
-              process.env.APPWRITE_DATABASE_ID!,
-              process.env.APPWRITE_TABLE_MESSAGES_ID!,
-              ID.unique(),
-              {
-                chat: chat.documents[0].$id,
-                message: req.body.message.text,
-              }
-            );
-
-            log('add message to user chat');
-          } else {
-            error('No User Found');
-            bot.telegram.sendMessage(
-              String(req.body.message.chat.id),
-              "i'm curious to get to know you, but to interact with you, you'll need to say the magic word! 😉 What are you waiting for? 😄"
-            );
-          }
-      }
-    } else {
-      console.log(req.body.chat);
-      if (req.body.action) {
-        const action = JSON.parse(req.body.action);
-        console.log(JSON.stringify(action));
-        console.log(action.module);
-        console.log(action.channel);
-        console.log(action.action);
-        if (
-          action.module === 'core' &&
-          action.action === 'talk' &&
-          action.channel === 'telegram'
-        ) {
-          log('add message in conversation');
-          log('connect to appwrite api');
-          log(JSON.stringify(req));
-          const client = new Client()
-            .setEndpoint(process.env.APPWRITE_ENDPOINT!)
-            .setProject(process.env.APPWRITE_PROJECT_ID!)
-            .setKey(process.env.APPWRITE_API_KEY!);
-          let datastore = new Databases(client);
+        } else {
+          bot.telegram.sendMessage(
+            String(req.body.message.chat.id),
+            'Welcome Back to Giulia BOT'
+          );
+          log(`user already in database`);
+        }
+        break;
+      default:
+        if (chat.total > 0) {
           datastore.createDocument(
             process.env.APPWRITE_DATABASE_ID!,
             process.env.APPWRITE_TABLE_MESSAGES_ID!,
             ID.unique(),
             {
-              message: action.payload.value,
-              bot: true,
-              chat: req.body.thought.message.chat.$id
+              chat: chat.documents[0].$id,
+              message: req.body.message.text,
             }
           );
-          log('connect to Telegram Bot');
-          const bot = new Telegraf(process.env.TELEGRAM_TOKEN!);
-          log(`sent message to telegram channel`);
-          bot.telegram.sendMessage(
-            String(action.payload.chat_id),
-            action.payload.value
-          );
+
+          log('add message to user chat');
         } else {
-          const bot = new Telegraf(process.env.TELEGRAM_TOKEN_ACTION!);
-          log(`sent action to telegram channel`);
+          error('No User Found');
           bot.telegram.sendMessage(
-            String(req.body.thought.message.chat.chat_id),
-            req.body.action
+            String(req.body.message.chat.id),
+            "i'm curious to get to know you, but to interact with you, you'll need to say the magic word! 😉 What are you waiting for? 😄"
           );
         }
+    }
+  } else {
+    console.log(req.body.chat);
+    if (req.body.action) {
+      const action = JSON.parse(req.body.action);
+      console.log(JSON.stringify(action));
+      console.log(action.module);
+      console.log(action.channel);
+      console.log(action.action);
+      if (
+        action.module === 'core' &&
+        action.action === 'talk' &&
+        action.channel === 'telegram'
+      ) {
+        log('add message in conversation');
+        log('connect to appwrite api');
+        log(JSON.stringify(req));
+        const client = new Client()
+          .setEndpoint(process.env.APPWRITE_ENDPOINT!)
+          .setProject(process.env.APPWRITE_PROJECT_ID!)
+          .setKey(process.env.APPWRITE_API_KEY!);
+        let datastore = new Databases(client);
+        const message: Models.DocumentList<any> =
+          await datastore.listDocuments(
+            process.env.APPWRITE_DATABASE_ID!,
+            process.env.APPWRITE_TABLE_TOUGHTS_ID!,
+            [Query.equal('message', req.body.message.chat.$id)]
+          );
+        datastore.createDocument(
+          process.env.APPWRITE_DATABASE_ID!,
+          process.env.APPWRITE_TABLE_MESSAGES_ID!,
+          ID.unique(),
+          {
+            message: action.payload.value,
+            bot: true,
+            chat: req.body.thought.message.chat.$id,
+          }
+        );
+        log('connect to Telegram Bot');
+        const bot = new Telegraf(process.env.TELEGRAM_TOKEN!);
+        log(`sent message to telegram channel`);
+        bot.telegram.sendMessage(
+          String(action.payload.chat_id),
+          action.payload.value
+        );
       } else {
-        error('api key not is valid');
+        const bot = new Telegraf(process.env.TELEGRAM_TOKEN_ACTION!);
+        log(`sent action to telegram channel`);
+        bot.telegram.sendMessage(
+          String(req.body.thought.message.chat.chat_id),
+          req.body.action
+        );
       }
+    } else {
+      error('api key not is valid');
     }
-    if (req.method === 'GET') {
-      return res.send('Silicia - Giul-IA BOT - telegram gateway');
-    }
- /* } catch (e: any) {
+  }
+  if (req.method === 'GET') {
+    return res.send('Silicia - Giul-IA BOT - telegram gateway');
+  }
+  /* } catch (e: any) {
     error(JSON.stringify(e));
   }*/
   return res.empty();
