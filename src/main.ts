3c821827c5c3e73613909865498e6dce07053b8e
Francesco Bozzotta (aka xFr4xx) <francesco.bozzotta@gmail.com>
2024-08-09 15:04:18 +0200

    xxx

--- src/main.ts
+++ src/main.ts
@@ -102,7 +102,7 @@ export default async ({ req, res, log, error }: Context) => {
     }
   } 
   debug(`request: ${JSON.stringify(req.body)}`);
-  try {
+  //try {
     const headers = { Authorization: 'Bearer ' + process.env.HA_TOKEN! };
     //log(JSON.stringify(req));
     //const req = {"bodyRaw":"{\"action\":\"{\\\"module\\\":\\\"home-assistant\\\",\\\"action\\\":\\\"get\\\",\\\"channel\\\":\\\"lights\\\"}\",\"state\":\"waiting\",\"$id\":\"66b3c77a0034311279e4\",\"$permissions\":[],\"$createdAt\":\"2024-08-07T19:14:03.457+00:00\",\"$updatedAt\":\"2024-08-07T19:14:03.457+00:00\",\"thought\":{\"thought\":\"{\\\"situation\\\":\\\"L'utente xFr4xx mi ha chiesto di accendere la luce del bagno.\\\",\\\"interpretation\\\":\\\"L'utente desidera che io interagisca con l'ambiente tramite Home Assistant.\\\",\\\"reflection\\\":\\\"Devo verificare se ho le informazioni necessarie per eseguire l'azione. Se non ho il nome dell'entit\\u00e0 'luce del bagno', dovr\\u00f2 chiedere maggiori dettagli.\\\"}\",\"$id\":\"66b3af380029da08ba04\",\"$createdAt\":\"2024-08-07T17:30:32.747+00:00\",\"$updatedAt\":\"2024-08-07T19:13:21.133+00:00\",\"$permissions\":[],\"message\":{\"message\":\"puoi accendere la luce del bagno ?\",\"bot\":false,\"$id\":\"66b3af330005bc8fb2ea\",\"$createdAt\":\"2024-08-07T17:30:27.171+00:00\",\"$updatedAt\":\"2024-08-07T17:30:32.768+00:00\",\"$permissions\":[],\"$databaseId\":\"66ae9f440015b50a678b\",\"$collectionId\":\"66aea08e003d46e738fa\"},\"chat\":{\"channel\":\"telegram\",\"chat_id\":\"7045034835\",\"$id\":\"66b27ff2130cac12b5c0\",\"$createdAt\":\"2024-08-06T19:56:34.092+00:00\",\"$updatedAt\":\"2024-08-06T19:58:07.348+00:00\",\"$permissions\":[],\"$databaseId\":\"66ae9f440015b50a678b\",\"$collectionId\":\"66ae9fc00025ac4542d6\"},\"$databaseId\":\"66ae9f440015b50a678b\",\"$collectionId\":\"66aea1a7003cba1f2b86\"},\"$databaseId\":\"66ae9f440015b50a678b\",\"$collectionId\":\"66b1f8ea001d5b79ca27\"}","body":{"action":"{\"module\":\"home-assistant\",\"action\":\"set\", \"payload\" : {\"channel\":\"light.kitchen_lights\", \"value\":\"on\"}}","state":"waiting","$id":"66b3c77a0034311279e4","$permissions":[],"$createdAt":"2024-08-07T19:14:03.457+00:00","$updatedAt":"2024-08-07T19:14:03.457+00:00","thought":{"thought":"{\"situation\":\"L'utente xFr4xx mi ha chiesto di accendere la luce del bagno.\",\"interpretation\":\"L'utente desidera che io interagisca con l'ambiente tramite Home Assistant.\",\"reflection\":\"Devo verificare se ho le informazioni necessarie per eseguire l'azione. Se non ho il nome dell'entità 'luce del bagno', dovrò chiedere maggiori dettagli.\"}","$id":"66b3af380029da08ba04","$createdAt":"2024-08-07T17:30:32.747+00:00","$updatedAt":"2024-08-07T19:13:21.133+00:00","$permissions":[],"message":{"message":"puoi accendere la luce del bagno ?","bot":false,"$id":"66b3af330005bc8fb2ea","$createdAt":"2024-08-07T17:30:27.171+00:00","$updatedAt":"2024-08-07T17:30:32.768+00:00","$permissions":[],"$databaseId":"66ae9f440015b50a678b","$collectionId":"66aea08e003d46e738fa"},"chat":{"channel":"telegram","chat_id":"7045034835","$id":"66b27ff2130cac12b5c0","$createdAt":"2024-08-06T19:56:34.092+00:00","$updatedAt":"2024-08-06T19:58:07.348+00:00","$permissions":[],"$databaseId":"66ae9f440015b50a678b","$collectionId":"66ae9fc00025ac4542d6"},"$databaseId":"66ae9f440015b50a678b","$collectionId":"66aea1a7003cba1f2b86"},"$databaseId":"66ae9f440015b50a678b","$collectionId":"66b1f8ea001d5b79ca27"},"headers":{"host":"66b3c6d57f9f6:3000","user-agent":"Appwrite/1.5.7","content-type":"application/json","x-appwrite-trigger":"event","x-appwrite-event":"databases.66ae9f440015b50a678b.collections.66b1f8ea001d5b79ca27.documents.66b3c77a0034311279e4.create","x-appwrite-user-id":"65eee35b6bf7806d9eb5","connection":"keep-alive","content-length":"1468"},"method":"POST","host":"66b3c6d57f9f6","scheme":"http","query":{},"queryString":"","port":3000,"url":"http://66b3c6d57f9f6:3000/","path":"/"}
@@ -143,7 +143,7 @@ export default async ({ req, res, log, error }: Context) => {
                 process.env.APPWRITE_TABLE_MESSAGES_ID!,
                 ID.unique(),
                 {
-                  message: `{ 'module': 'home-assistant', 'action': 'input', 'channel': 'lights', 'value': '${JSON.stringify(entities)}' }`,
+                  message: `{ 'module': 'home-assistant', 'action': 'input', 'channel': 'lights', 'payload': ('value': '${JSON.stringify(entities)}' }}`,
                   bot: false,
                   chat: req.body.thought.chat.$id,
                 }
@@ -176,7 +176,7 @@ export default async ({ req, res, log, error }: Context) => {
       return res.send('Silicia - Giul-IA BOT - home assistant module');
     }
     return res.empty();
-  } catch (e: any) {
-    error(e);
-  }
+  /*} catch (e: any) {
+    error(String(e));
+  }*/
 };
